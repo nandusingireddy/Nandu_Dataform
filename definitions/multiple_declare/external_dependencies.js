e24f9436e0b653234509dd36f80408d2a8782b5e
Harika <Harika.Siripurapu@Bilvantis.io>
2024-02-27 16:45:50 +0000

    modify

---
+++ definitions/multiple_declare/external_dependencies.js
@@ -0,0 +1,13 @@
+declare({
+    database: "secure-potion-409606",
+    schema: "NanduSingireddy",
+    name: "SQLServer"
+});
+
+declare({
+    database: "secure-potion-409606",
+    schema: "NanduSingireddy",
+    name: "info_1"
+});
+
+
